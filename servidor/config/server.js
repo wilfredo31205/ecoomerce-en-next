9543019e4fa75c8f27677d70fe358d6cc794dde8
wilfredo <wilfredofrias146@gmail.com>
2021-07-16 13:21:15 -0400

    prueba

---
+++ servidor/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '229393304c5eefc1513f06ae942335a9'),
+    },
+  },
+});
---
+++ servidor/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0d64d34a-d238-4533-b98f-fa8eea9605a3'
+};
